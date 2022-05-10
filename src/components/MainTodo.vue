55ffda83ccfe3b0c13fdbac096c5b76566557926
github-test13 <84000918+github-test13@users.noreply.github.com>
2022-05-10 21:14:08 +0900

    9章 ロジックの分離

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,69 +1,38 @@
 <script setup>
 import { ref } from 'vue';
+import { useTodoList } from '/src/composables/useTodoList.js';
+
 const todoRef = ref('');
-
-// ローカルストレージにtodoListRefが存在すればparseし、
-// 存在しなければundefinedになるため、空配列をセットする
-const todoListRef = ref([]);
-const todoList = localStorage.todoList;
-todoListRef.value = todoList ? JSON.parse(todoList) : [];
-
-// 編集ボタンを押下したときにtrueにする
 const isEditRef = ref(false);
-let editId = -1;
+const { todoListRef, add, edit, show, del, check } = useTodoList();
 
 // 追加ボタン
 const addTodo = () => {
-  // ミリ秒
-  const id = new Date().getTime();
-  // 入力されたTODOを配列に格納
-  todoListRef.value.push({ id: id, task: todoRef.value });
-  // ローカルストレージに格納
-  localStorage.todoList = JSON.stringify(todoListRef.value);
-  // 格納後は入力欄を空にする
+  add(todoRef.value);
   todoRef.value = '';
 };
 
 // 変更ボタン
 const editTodo = () => {
-  // 編集対象のTODOを取得する
-  const todo = todoListRef.value.find((todo) => todo.id === editId);
-  // TODOリストから編集対象のインデックスを取得
-  const idx = todoListRef.value.findIndex((todo) => todo.id === editId);
-  // taskを編集後のTODOで置き換える
-  todo.task = todoRef.value;
-  // splice関数でインデックスを元に対象オブジェクトを置き換える
-  todoListRef.value.splice(idx, 1, todo);
-  // ローカルストレージに格納
-  localStorage.todoList = JSON.stringify(todoListRef.value);
+  edit(todoRef.value);
   isEditRef.value = false;
-  editId = -1;
-  // 格納後は入力欄を空にする
   todoRef.value = '';
 };
 
 // 編集ボタン
 const showTodo = (id) => {
-  // 配列（todoListRef.value）から引数のidと同じ要素を検索する。
-  // findの「(todo)」には配列の要素が引数として順番に入る。
-  // 「todo.id === id」がtrueなら、その時点の要素（todo）が返る。
-  const todo = todoListRef.value.find((todo) => todo.id === id);
-  // 取得した要素からtaskを取り出す
-  todoRef.value = todo.task;
+  todoRef.value = show(id);
   isEditRef.value = true;
-  editId = id;
 };
 
 // 削除ボタン
 const deleteTodo = (id) => {
-  const todo = todoListRef.value.find((todo) => todo.id === id);
-  const idx = todoListRef.value.findIndex((todo) => todo.id === id);
+  del(id);
+};
 
-  const delMsg = '「' + todo.task + '」を削除しますか？';
-  if (!confirm(delMsg)) return;
-
-  todoListRef.value.splice(idx, 1);
-  localStorage.todoList = JSON.stringify(todoListRef.value);
+// チェックボックス
+const changeCheck = (id) => {
+  check(id);
 };
 </script>
 
---
+++ src/composables/useTodoList.js
@@ -0,0 +1,67 @@
+import { ref } from 'vue';
+
+export const useTodoList = () => {
+  // ローカルストレージにtodoListRefが存在すればparseし、
+  // 存在しなければundefinedになるため、空配列をセットする
+  const todoListRef = ref([]);
+  const todoList = localStorage.todoList;
+  todoListRef.value = todoList ? JSON.parse(todoList) : [];
+
+  // TODOリストからidを元にTODO情報を取得
+  const findById = (id) => {
+    return todoListRef.value.find((todo) => todo.id === id);
+  };
+
+  // TODOリストからidを元にTODO情報のインデックスを取得
+  const findIndexById = (id) => {
+    return todoListRef.value.findIndex((todo) => todo.id === id);
+  };
+
+  // 追加ボタン処理
+  const add = (task) => {
+    const id = new Date().getTime();
+    todoListRef.value.push({ id: id, task: task });
+    localStorage.todoList = JSON.stringify(todoListRef.value);
+  };
+
+  // 変更ボタン処理
+  const edit = (task) => {
+    const todo = findById(editId.value);
+    const idx = findIndexById(editId.value);
+    todo.task = task;
+    todoListRef.value.splice(idx, 1, todo);
+    localStorage.todoList = JSON.stringify(todoListRef.value);
+    editId.value = -1;
+  };
+
+  // 編集ボタン処理
+  const editId = ref(-1);
+  const show = (id) => {
+    const todo = findById(id);
+    editId.value = id;
+    return todo.task;
+  };
+
+  // 削除ボタン処理
+  const del = (id) => {
+    const todo = findById(id);
+
+    const delMsg = '「' + todo.task + '」を削除しますか？';
+    if (!confirm(delMsg)) return;
+
+    const idx = findIndexById(id);
+    todoListRef.value.splice(idx, 1);
+    localStorage.todoList = JSON.stringify(todoListRef.value);
+  };
+
+  // チェックボックス処理
+  const check = (id) => {
+    const todo = findById(id);
+    const idx = findIndexById(id);
+    todo.checked = !todo.checked;
+    todoListRef.value.splice(idx, 1, todo);
+    localStorage.todoList = JSON.stringify(todoListRef.value);
+  };
+
+  return { todoListRef, add, edit, show, del, check };
+};
